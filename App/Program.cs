e18d5bec9ff91c6814bb95e12b339394eadf002a
Sukhpreet Singh <sukhpreetsingh19289@gmail.com>
2025-04-10 15:10:26 +0000

    variables

--- App/Program.cs
+++ App/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// 
+int age = 25;
+string name = "Joe";
+
+Console.WriteLine("Name : " + name );
+
+Console.WriteLine("Age : " + age);
--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba41a9c40c7a78fc9066f18fb66ac5a52d9109ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07630536f5377fd8bd7114c2c511f242bf205a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
